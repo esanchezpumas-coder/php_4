390a57cf520c55bdb47dbd409ed72151b345ffce
esanchezpumas-coder <esanchezpumas@gmail.com>
2025-11-25 22:38:17 +0000

    Agregada vista de Ponente

---
+++ src/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+use App\Http\Controllers\PonenteVistaController;
+Route::get('/ponentes-vista',[PonenteVistaController::class,'index'])->name('ponentes.vista');
+
+Route::post('/ponentes-vista',[PonenteVistaController::class,'store'])->name('ponentes.store');
+Route::delete('/ponentes-vista/{id}', [PonenteVistaController::class, 'destroy'])->name('ponentes.destroy');
+
